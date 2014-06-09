1b2d4e7c2cb558e4617813981ef7674cd3cb1081
Xenocider <xenocider.dev@gmail.com>
2014-06-09 10:34:32 -0700

    Started Forge Crafting
    + Created and finished forge crafting gui
    + Forging swords and axes should work

--- src/io/github/xenocider/celestialrift/crafting/CraftingGui.java
+++ src/io/github/xenocider/celestialrift/crafting/CraftingGui.java
@@ -31,6 +31,8 @@ public class CraftingGui implements Listener {
 	public static Inventory bowGui = Bukkit.createInventory(null, InventoryType.FURNACE, "Woodworking Table: Bow");
 	public static Inventory staffGui = Bukkit.createInventory(null, InventoryType.FURNACE, "Woodworking Table: Staff");
 	public static Inventory plankGui = Bukkit.createInventory(null, InventoryType.FURNACE, "Woodworking Table: Planks");
+	public static Inventory swordGui = Bukkit.createInventory(null, InventoryType.FURNACE, "Ancient Forge: Swords");
+	public static Inventory axeGui = Bukkit.createInventory(null, InventoryType.FURNACE, "Ancient Forge: Axes");
 
 	public static ItemStack Gem_Fire = createItem(Material.GREEN_RECORD,1,"Amber", new ArrayList<String>(Arrays.asList("It seems to have some special proerties")));
 	public static ItemStack Gem_Ice = createItem(Material.GREEN_RECORD,1,"Saphire", new ArrayList<String>(Arrays.asList("It seems to have some special proerties")));
@@ -56,6 +58,19 @@ public class CraftingGui implements Listener {
 		createDisplay(Material.BOW, 1, inv, 3, "Craft a Bow", "Click this to craft a bow");
 		createDisplay(Material.WOOD_HOE, 1, inv, 4, "Craft a Staff", "Click this to craft a staff");
 		createDisplay(Material.WOOD, 1, inv, 5, "Craft some Planks", "Click this to craft some planks");
+		p.openInventory(inv);
+	}
+	
+	private static void openForgeGui(Player p) {
+		Inventory inv = Bukkit.createInventory(p, 9, "Ancient Forge");
+		createDisplay(Material.IRON_SWORD, 1, inv, 3, "Forge a Sowrd", "Click this to forge a sword");
+		createDisplay(Material.IRON_AXE, 1, inv, 4, "Forge an Axe", "Click this to forge an axe");
+		createDisplay(Material.FURNACE, 1, inv, 5, "Smelt some Ingots", "Click this to smelt some ingots");
+		createDisplay(Material.IRON_HELMET, 1, inv, 4, "Forge a Helmet", "Click this to forge a helmet");
+		createDisplay(Material.IRON_CHESTPLATE, 1, inv, 4, "Forge a Chestplate", "Click this to forge a chestplate");
+		createDisplay(Material.IRON_LEGGINGS, 1, inv, 4, "Forge some Leggings", "Click this to forge some leggings");
+		createDisplay(Material.IRON_BOOTS, 1, inv, 4, "Forge some Boots", "Click this to forge some boots");
+		createDisplay(Material.GOLD_NUGGET, 1, inv, 4, "Forge some Rings", "Click this to forge some rings");
 		p.openInventory(inv);
 	}
 	
@@ -103,6 +118,35 @@ public class CraftingGui implements Listener {
 			event.setCancelled(true);
 		}
 		
+		//Forge Gui Clicks
+		if (inv.getName().equals(forgeGui.getName())) {
+			if (event.getCurrentItem().getType() == Material.IRON_SWORD) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Swords"));
+			}
+			else if (event.getCurrentItem().getType() == Material.IRON_AXE) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Axes"));
+			}
+			else if (event.getCurrentItem().getType() == Material.IRON_HELMET) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Helmets"));
+			}
+			else if (event.getCurrentItem().getType() == Material.IRON_CHESTPLATE) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Chestplates"));
+			}			
+			else if (event.getCurrentItem().getType() == Material.IRON_LEGGINGS) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Leggings"));
+			}			
+			else if (event.getCurrentItem().getType() == Material.IRON_BOOTS) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Boots"));
+			}			
+			else if (event.getCurrentItem().getType() == Material.FURNACE) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Smelting"));
+			}			
+			else if (event.getCurrentItem().getType() == Material.GOLD_NUGGET) {
+				player.openInventory(Bukkit.createInventory(player, InventoryType.FURNACE, "Ancient Forge: Rings"));
+			}
+			event.setCancelled(true);
+		}
+		
 		//Crafting Gui Clicks
 		if (inv.getType() == InventoryType.FURNACE) {
 			if (event.getSlotType() == SlotType.FUEL || event.getSlotType() == SlotType.CRAFTING) {
@@ -129,10 +173,12 @@ public class CraftingGui implements Listener {
 	}
 
 	private void updateCraftGui(final Player player, Inventory inv) {
-		// TODO Auto-generated method stub
 		int resources = 0;
 		String gemType = null;
 		
+		
+		//Weapon crafting
+		if (inv.getName().equals(bowGui.getName()) || inv.getName().equals(staffGui.getName()) || inv.getName().equals(swordGui.getName()) || inv.getName().equals(axeGui.getName())) {
 		if (inv.getName().equals(bowGui.getName()) || inv.getName().equals(staffGui.getName())) {
 			if (inv.getItem(0) != null && inv.getItem(0).getType() == Material.WOOD) {
 				resources = resources + inv.getItem(0).getAmount();
@@ -140,6 +186,16 @@ public class CraftingGui implements Listener {
 			if (inv.getItem(1) != null && inv.getItem(1).getType() == Material.WOOD) {
 				resources = resources + inv.getItem(1).getAmount();
 			}
+		}
+		else if (inv.getName().equals(swordGui.getName()) || inv.getName().equals(axeGui.getName())) {
+			if (inv.getItem(0) != null && inv.getItem(0).getType() == Material.IRON_INGOT) {
+				resources = resources + inv.getItem(0).getAmount();
+			}
+			if (inv.getItem(1) != null && inv.getItem(1).getType() == Material.IRON_INGOT) {
+				resources = resources + inv.getItem(1).getAmount();
+			}
+		}
+
 			if (inv.getItem(0) != null && inv.getItem(0).getType() == Material.GREEN_RECORD) {
 				ItemStack item = inv.getItem(0);
 				if (item.getItemMeta().getDisplayName() == Gem_Fire.getItemMeta().getDisplayName()) {
@@ -165,7 +221,7 @@ public class CraftingGui implements Listener {
 				}
 			}
 			if (resources >= 1) {
-				ItemStack item;
+				ItemStack item = null;
 				int level = (int) Math.pow(resources, (1.0/exponent));
 				if (level > 32) {level = 32;}
 				int minDam = 6+2*level;
@@ -174,18 +230,26 @@ public class CraftingGui implements Listener {
 					if (gemType != null) {item = createItem(Material.BOW, 1, "Crafted Bow", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + minDam + "-" + maxDam,"Gem: " + gemType,"Crafted by: " + player.getDisplayName())));}
 					else {item = createItem(Material.BOW, 1, "Crafted Bow", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + minDam + "-" + maxDam,"Crafted by: " + player.getDisplayName())));}
 				}
-				else {
+				else if (inv.getName().equals(staffGui.getName())) {
 					if (gemType != null) {item = createItem(Material.WOOD_HOE, 1, "Crafted Staff", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + minDam + "-" + maxDam,"Gem: " + gemType,"Crafted by: " + player.getDisplayName())));}
 					else {item = createItem(Material.WOOD_HOE, 1, "Crafted Staff", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + minDam + "-" + maxDam,"Crafted by: " + player.getDisplayName())));}
+				}
+				else if (inv.getName().equals(swordGui.getName())) {
+					if (gemType != null) {item = createItem(Material.IRON_SWORD, 1, "Crafted Sword", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + (int) minDam/2 + "-" + (int) maxDam/2,"Gem: " + gemType,"Crafted by: " + player.getDisplayName())));}
+					else {item = createItem(Material.IRON_SWORD, 1, "Crafted Sword", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + (int) minDam/2 + "-" + (int) maxDam/2,"Crafted by: " + player.getDisplayName())));}
+				}
+				else if (inv.getName().equals(axeGui.getName())) {
+					if (gemType != null) {item = createItem(Material.IRON_AXE, 1, "Crafted Axe", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + (int) minDam/2 + "-" + (int) maxDam/2,"Gem: " + gemType,"Crafted by: " + player.getDisplayName())));}
+					else {item = createItem(Material.IRON_AXE, 1, "Crafted Axe", new ArrayList<String>(Arrays.asList("Level: " + level,"Dmg: " + (int) minDam/2 + "-" + (int) maxDam/2,"Crafted by: " + player.getDisplayName())));}
 				}
 				inv.setItem(2, item);
 			}
 			else {
 				inv.clear(2);
 			}
-//TODO
 		}
 		
+		//Resource Crafting
 		if (inv.getName().equals(plankGui.getName())) {
 			if (inv.getItem(0) != null && inv.getItem(0).getType() == Material.LOG) {
 				resources = resources + inv.getItem(0).getAmount();
@@ -256,6 +320,30 @@ public class CraftingGui implements Listener {
 				}
 			}
 			
+			//Forge weapons
+			if (inv.getName().equals(swordGui.getName()) || inv.getName().equals(axeGui.getName())) {
+				if (inv.getItem(0) != null && inv.getItem(0).getType() == Material.IRON_INGOT) {
+					resources = resources + inv.getItem(0).getAmount();
+				}
+				if (inv.getItem(1) != null && inv.getItem(1).getType() == Material.IRON_INGOT) {
+					resources = resources + inv.getItem(1).getAmount();
+				}
+				int level = (int) Math.pow(resources, (1.0/exponent));
+				int cost = (int) Math.pow(level, exponent);
+				
+				ItemStack extra = new ItemStack(Material.IRON_INGOT,resources - cost);
+				ItemStack item = inv.getItem(2);
+				
+				player.closeInventory();
+				
+				//Try to add the items to the player's inventory, or else it will drop the remaining on the ground
+				HashMap<Integer, ItemStack> nope = player.getInventory().addItem(item,extra);
+				for(Entry<Integer, ItemStack> entry : nope.entrySet())
+				{   
+					player.getWorld().dropItemNaturally(player.getLocation(), entry.getValue());
+				}
+			}
+			
 			Bukkit.getServer().getScheduler()
 	        .scheduleSyncDelayedTask(plugin, new Runnable() {
 	            @Override
@@ -276,7 +364,10 @@ public class CraftingGui implements Listener {
 				openWoodworkGui(event.getPlayer());
 				event.setCancelled(true);
 			}
-			//if (event.getClickedBlock().getType() == Material.FURNACE) {event.getPlayer().openInventory(CraftingGui.forgeGui);}
+			if (event.getClickedBlock().getType() == Material.FURNACE) {
+				openForgeGui(event.getPlayer());
+				event.setCancelled(true);
+			}
 			//if (event.getClickedBlock().getType() == Material.CAULDRON) {event.getPlayer().openInventory(CraftingGui.ovenGui);}
 			//if (event.getClickedBlock().getType() == Material.ANVIL) {event.getPlayer().openInventory(CraftingGui.anvilGui);}
 		}
